7036d4d00559c036a7dc05397b747718cc85afc7
Vanshikaa04 <wadhwanivanshika86@gmail.com>
2025-04-22 11:20:23 +0530

    CORS UPDATED

--- backend/testMongo.js
+++
@@ -1,7 +0,0 @@
-import mongoose  from "mongoose";
-const uri = "mongodb+srv://wadhwanivanshika86:62915297@cluster0.cbjlglh.mongodb.net/test?retryWrites=true&w=majority'";
-
-
-mongoose.connect(uri)
-  .then(() => console.log('✅ MongoDB Connected!'))
-  .catch(err => console.error('❌ MongoDB Connection Error:', err));
